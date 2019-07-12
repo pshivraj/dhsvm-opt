2b893adc4e87a610a0796a8f6abe1f31ca0d4c93
pshivraj <shivrajpurshottam@gmail.com>
2019-07-12 12:45:50 -0700

    added helped function to chnage config setting

--- scripts/fast/fast.py
+++ scripts/fast/fast.py
@@ -89,3 +89,11 @@ class fast_run_setup(object):
         except Exception as e:
             logging.info('Exception occured: %s', str(e))
         return model_fit
+
+#Function to write to DHSVM config file simulated inputs from DREAM.
+def change_setting(config_file, setting_name, new_value, occurrence_loc='g'):
+    sed_cmd = "sed -i 's:{setting_name} = .*:{setting_name} = {new_value}:{occurrence_loc}' {config_file}"
+    sed_cmd = sed_cmd.format(setting_name = setting_name, new_value = new_value
+                             , config_file = config_file
+                             , occurrence_loc = occurrence_loc)
+    return subprocess.call(sed_cmd, shell=True)
