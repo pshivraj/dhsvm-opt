fa06f58e706062ffbd0726fcb99945adf798d997
Shivraj <shivrajpurshottam@gmail.com>
2019-07-13 20:04:26 -0700

    Fix C compiler not found

--- scripts/sql-setup/sql-setup.sh
+++ scripts/sql-setup/sql-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+apt-get install build-essential
 wget http://www.sqlite.org/sqlite-autoconf-3070603.tar.gz &&
 tar xvfz sqlite-autoconf-3070603.tar.gz &&
 cd sqlite-autoconf-3070603 &&
+#apt-get install build-essential &&
 ./configure &&
 sed -e '/DEFS =/s/.*/& -DSQLITE_MAX_COLUMN=32767/' Makefile &&
 make &&
