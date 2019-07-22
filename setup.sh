f98b079ec3116db2c9f56a3fea4bbc860beb56a5
Shivraj <shivrajpurshottam@gmail.com>
2019-07-21 18:29:41 -0700

    Fix base env

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda.sh -b -u &&
 echo 'export PATH=~/miniconda3/bin:$PATH' >> ~/.bashrc &&
 source ~/.bashrc
-conda init bash
+cd scripts/sql-setup && bash sql-setup.sh && cd ../..
 conda create -n dhsvm-opt python=3.6 -y &&
 conda activate dhsvm-opt
-cd scripts/sql-setup && bash sql-setup.sh && cd ../..
