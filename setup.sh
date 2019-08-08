87b1fb455a8934326f3e5342624891b22e9189e4
Shivraj <shivrajpurshottam@gmail.com>
2019-08-07 17:52:11 -0700

    update to have custom spotpy

--- setup.sh
+++ setup.sh
@@ -8,4 +8,6 @@ cd scripts/sql-setup && bash sql-setup.sh && cd ../..
 conda init bash
 source ~/.bashrc
 conda create -n dhsvm-opt -y &&
-conda activate dhsvm-opt
+conda activate dhsvm-opt &&
+git clone https://github.com/pshivraj/spotpy.git && cd spotpy && python setup.py install
+
