e74ebbe812f02558008de5d863409db3e95c735f
Shivraj <shivrajpurshottam@gmail.com>
2019-08-18 22:40:26 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,9 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda.sh -b -u &&
 echo 'export PATH=~/miniconda3/bin:$PATH' >> ~/.bashrc &&
 source ~/.bashrc
+source ~/miniconda3/etc/profile.d/conda.sh
 conda create -n dhsvm-opt -y &&
-source activate dhsvm-opt &&
+conda activate dhsvm-opt &&
 cd scripts/sql-setup && bash sql-setup.sh && cd ../..
 rm -rf spotpy &&
 git clone https://github.com/pshivraj/spotpy.git && cd spotpy && python setup.py install
