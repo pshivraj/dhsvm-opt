e6967a4d06846ecbbe6007bbad66e3711d135172
Shivraj <shivrajpurshottam@gmail.com>
2019-08-18 20:59:47 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,14 +3,13 @@
 set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-  
+
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -u &&
 echo 'export PATH=~/miniconda3/bin:$PATH' >> ~/.bashrc &&
 source ~/.bashrc
-cd scripts/sql-setup && bash sql-setup.sh && cd ../..
-conda init bash
-source ~/.bashrc
 conda create -n dhsvm-opt -y &&
-conda activate dhsvm-opt &&
+source activate dhsvm-opt &&
+cd scripts/sql-setup && bash sql-setup.sh && cd ../..
+rm -rf spotpy &&
 git clone https://github.com/pshivraj/spotpy.git && cd spotpy && python setup.py install
