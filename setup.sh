b81f08b2ccf8540469cd73a09e64bc268d715ad4
Shivraj <shivrajpurshottam@gmail.com>
2019-08-18 22:16:20 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
+chmod a+x ~/.bashrc
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -u &&
 echo 'export PATH=~/miniconda3/bin:$PATH' >> ~/.bashrc &&
